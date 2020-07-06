89bf2543a4d8989c7ad7147e9a067dbe49b9e3b9
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2020-07-06 10:49:12 +0800

    Create alb_listener_rule.tf

---
+++ alb_listener_rule.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_listener_rule" "this" {
+  listener_arn = var.listener_arn
+  priority     = var.priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+
+  condition {
+    path_pattern {
+      values = [var.path_pattern]
+    }
+  } 
+}
